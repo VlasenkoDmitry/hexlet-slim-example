ccea135375651493858fb1a8256d520fce15b151
DzmitryV <vlasenko.dmitry1@gmail.com>
2024-03-16 12:50:40 +0300

    lesson 12

--- public/index.php
+++ public/index.php
@@ -4,10 +4,66 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    // Параметром передается базовая директория, в которой будут храниться шаблоны
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
+
+
+
+$users = ['mike', 'mishel', 'adel', 'keks', 'kamila'];
+
+$app->get('/user/{nick}', function ($request, $response, $args) {
+    $params = [
+        'nick' => $args["nick"]
+    ];
+    return $this->get('renderer')->render($response, 'user/index.phtml', $params);
+});
+
+$app->get('/users', function ($request, $response) use ($users) {
+    $term = $request->getQueryParam("user");
+    $result = array_filter($users, fn($elem) => strpos($elem, $term) !== false);
+    $params = [
+        'users' => $result,
+        'term' => $term
+    ];
+    return $this->get('renderer')->render($response, 'users/index.phtml', $params);
+});
+
+$app->get('/users/{id}', function ($request, $response, $args) use ($users) {
+    $id = $args["id"];
+    $userArray = [];
+    foreach ($users as $user) {
+        if ($user["id"] == $id) {
+            $userArray[] = $user;
+        }
+    }
+    $params = [
+        "users" => $userArray
+    ];
+    return $this->get('renderer')->render($response, 'users/show.phtml', $params);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 $app->get('/', function ($request, $response) {
